5e092f25f9c733c9171b0ae3be4c35b94bb8b36a
Mick Lin <cl3469@columbia.edu>
2025-09-09 13:18:03 -0700

    use goroutines group.
    Test plan:
    2025-09-09 13:14:33 ticker
    2025-09-09 13:14:33 Consumer (new): 41:event-41
    2025-09-09 13:14:33 
    2025-09-09 13:14:33 Consumer (new): 42:event-42
    2025-09-09 13:14:33 
    2025-09-09 13:14:33 Consumer (new): 43:event-43
    2025-09-09 13:14:33 
    2025-09-09 13:14:33 Consumer (new): 44:event-44
    2025-09-09 13:14:33 
    2025-09-09 13:14:33 Consumer (new): 45:event-45
    2025-09-09 13:14:33 
    2025-09-09 13:14:33 Consumer (new): 46:event-46
    2025-09-09 13:14:33 
    2025-09-09 13:14:33 Consumer (new): 47:event-47
    2025-09-09 13:14:33 
    2025-09-09 13:14:33 Consumer (new): 48:event-48
    2025-09-09 13:14:33 
    2025-09-09 13:14:33 Consumer (new): 49:event-49
    2025-09-09 13:14:33 
    2025-09-09 13:14:33 All events produced and consumed. Check /data/wal.log

--- main.go
+++ main.go
@@ -2,31 +2,47 @@ package main
 
 import (
 	"fmt"
-        "time"
+	"sync"
+	"time"
 )
+
 func main() {
-    walPath := "/data/wal.log"
+	walPath := "/data/wal.log"
 
+	var wg sync.WaitGroup
 
-    ingestor, err := NewEventIngestor(walPath, 10, 501*time.Millisecond)
-    if err != nil {
-	panic(err)
-    }
-    ingestor.Start()
-    // Start file consumer
-    fc, err := NewFileConsumer(walPath)
-    if err != nil {
-	panic(err)
-    }
-    fc.Start()
-    go func() {
+	// Initialize EventIngestor
+	ingestor, err := NewEventIngestor(walPath, 10, 501*time.Millisecond)
+	if err != nil {
+		panic(err)
+	}
+	ingestor.Start() // internally launches flushing goroutine
+
+	// Initialize FileConsumer
+	fc, err := NewFileConsumer(walPath)
+	if err != nil {
+		panic(err)
+	}
+
+	// Add 2 to WaitGroup: consumer + producer
+	wg.Add(2)
+
+	// Start consumer
+	go func() {
+		defer wg.Done()
+		fc.Start()
+	}()
+
+	// Start producer
+	go func() {
+		defer wg.Done()
 		for i := 1; i <= 50; i++ {
 			ingestor.Push(Event{ID: i, Data: fmt.Sprintf("event-%d", i)})
 			time.Sleep(50 * time.Millisecond)
 		}
-    }()
-    	
-    // Wait to ensure flushing happens
-    time.Sleep(60 * time.Second)
-    fmt.Println("Done writing events. Check /data/wal.log")
+	}()
+
+	// Wait for both goroutines to finish
+	wg.Wait()
+	fmt.Println("All events produced and consumed. Check /data/wal.log")
 }
